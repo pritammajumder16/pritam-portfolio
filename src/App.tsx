0d83caac65609d7620b0570de6478cde0db50817
pritam16 <pritammajumder416@gmail.com>
2024-07-26 01:03:43 +0530

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <span className="text-3xl  font-bold underline">Hello world!</span>
+    </div>
+  );
 }
 
 export default App;
